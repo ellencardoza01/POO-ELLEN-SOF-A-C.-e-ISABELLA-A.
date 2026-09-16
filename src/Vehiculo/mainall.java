cd31cc2ac24863dd556cf902e453ab08dfcb2379
IDEADPAD SLIM 3F6LM <IDEADPAD SLIM 3F6LM@192.168.1.61>
2026-09-16 09:15:40 -0500

    proyecto actualizado 2

--- src/Vehiculo/mainall.java
+++ src/Vehiculo/mainall.java
@@ -3,12 +3,12 @@ package Vehiculo;
 public class mainall {
     public static void main(String[] args) {
 
-        Auto auto1 = new Auto("Toyota", "Corolla", 30.0);
+        Auto auto1 = new Auto("mercedes", "clase A", 8874.63);
         double costoAuto = auto1.calcularCostoAlquiler(5);
         System.out.println("Auto: " + auto1.getMarca() + " " + auto1.getModelo()
                 + " - Costo: " + costoAuto);
 
-        Moto moto1 = new Moto("Honda", "CB190R", 15.0);
+        Moto moto1 = new Moto("Honda", "CA45l", 95.000);
         double costoMoto = moto1.calcularCostoAlquiler(5);
         System.out.println("Moto: " + moto1.getMarca() + " " + moto1.getModelo()
                 + " - Costo: " + costoMoto);
